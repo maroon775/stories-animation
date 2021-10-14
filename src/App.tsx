05a511c9baf7e72d03a893e8e53cf0ab5ae045d5
Amal <maroon775@gmail.com>
2021-10-15 04:36:37 +0500

    html, css layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Header} from "./components/Header";
+import {StoryState, UserStory} from "./components/UserStory";
+const faker = require('faker');
+
+const profiles = (new Array(15)).fill('');
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Header/>
+            </header>
+
+            <div className="App-grid">
+                {profiles.map(() => {
+                    const userTitle = faker.name.findName();
+                    const userName = faker.internet.userName().toLowerCase();
+                    const avatar = faker.image.avatar();
+                    const id = faker.datatype.uuid();
+                    const state:StoryState = faker.random.number({min: 0, max: 3});
+                    return <UserStory state={state} userName={userName} userAvatar={avatar} userTitle={userTitle} storyId={id}/>
+                })}
+            </div>
+        </div>
+    );
 }
 
 export default App;
